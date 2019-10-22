045116a14f7a4d08505890f93d47f61a2da473b5
Lorena Jesus <lorena.laj@gmail.com>
2019-10-22 14:31:33 -0400

    Fixed error on formatting of entity

--- BPlusTree-BD-master/tp2/scr/parser/parser.cpp
+++ BPlusTree-BD-master/tp2/scr/parser/parser.cpp
@@ -11,52 +11,21 @@ using namespace std;
 
 Record setRecord(vector<string> row){
     try{
-        int idCurrent;
+        int idCurrent = stoi(row[0]);
         char tituloCurrent[MAX_TITULO];
-        int anoCurrent;
+        int anoCurrent = stoi(row[2]);
         char autoresCurrent[MAX_AUTORES];
-        int citacoesCurrent;
+        int citacoesCurrent = stoi(row[4]);
         char atualizacoesCurrent[MAX_ATUALIZACOES];  
         char snippetCurrent[MAX_SNIPPET];
 
-        // idCurrent = stoi(row[0]);
-        // strcpy(tituloCurrent, row[1].c_str());
-
-        // if(!(row[2] == "NULL")){       //Check if attribute is null
-        //     anoCurrent = stoi(row[2]);
-        // }
-        
-        // strcpy(autoresCurrent, row[3].c_str());
-
-        // if(!(row[4] == "NULL")){       //Check if attribute is null
-        //     citacoesCurrent = stoi(row[4]);
-        // }
-
-        // strcpy(atualizacoesCurrent, row[5].c_str());
-        // strcpy(snippetCurrent, row[6].c_str());
-
-        idCurrent = stoi(row[0]);
         strcpy(tituloCurrent, row[1].c_str());
-        anoCurrent = stoi(row[2]);
         strcpy(autoresCurrent, row[3].c_str());
-        citacoesCurrent = stoi(row[4]);
         strcpy(atualizacoesCurrent, row[5].c_str());
-        if(row[6] == "0"){
-            strcpy(snippetCurrent, "0");
-        }else{
-            strcpy(snippetCurrent, row[6].c_str());
-        }
+        strcpy(snippetCurrent, row[6].c_str());
+
         Record record = Record(idCurrent, tituloCurrent, anoCurrent, autoresCurrent, citacoesCurrent, atualizacoesCurrent, snippetCurrent);
-        //print_record(record);     
-        cout << "Id: " << record.id << endl;
-        cout << "Titulo: " << record.titulo << endl;
-        cout << "Ano: " << record.ano << endl;
-        cout << "Autores: " << record.autores << endl;
-        cout << "Citações: " << record.citacoes << endl;
-        cout << "Atualizações: " << record.atualizacoes << endl;
-        cout << "Snippet: " << record.snippet << endl;
-        cout << endl;
-        
+        // record.print();        
         return record;
 
     }catch(char *e){
@@ -99,23 +68,6 @@ void read_file(){
                 }   
             }
             vet.push_back(setRecord(row));      //Add new record to vector of records
-            // cout << "Id: " << idCurrent << endl;
-            // cout << "Titulo: " << tituloCurrent << endl;
-            // cout << "Ano: " << anoCurrent << endl;
-            // cout << "Autores: " << autoresCurrent << endl;
-            // cout << "Citações: " << citacoesCurrent << endl;
-            // cout << "Atualizações: " << atualizacoesCurrent << endl;
-            // cout << "Snippet: " << snippetCurrent << endl;
-            // cout << endl;
-            
-        //     cout << "Id: " << row[0] << endl;
-        //     cout << "Titulo: " << row[1] << endl;
-        //     cout << "Ano: " << row[2] << endl;
-        //     cout << "Autores: " << row[3] << endl;
-        //     cout << "Citações: " << row[4] << endl;
-        //     cout << "Atualizações: " << row[5] << endl;
-        //     cout << "Snippet: " << row[6] << endl;
-        //     cout << endl;
         }
     }
 }
