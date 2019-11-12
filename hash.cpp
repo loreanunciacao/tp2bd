67b61203c360402fead290a625ffc2c19cc5ea16
Lorena Anunciação de Jesus <lorena.laj@gmail.com>
2019-11-12 14:33:05 -0400

    Fixed parser and created block

---
+++ hash.cpp
@@ -0,0 +1,13 @@
+#ifndef HASH_CLASS
+#define HASH_CLASS
+
+#include "record.hpp"
+#include "block.hpp"
+
+void addToHash(Record record){
+
+    Block blk;
+    
+}
+
+#endif
---
+++ parser.hpp
@@ -0,0 +1,13 @@
+
+#ifndef PARSER_CLASS
+#define PARSER_CLASS
+
+#include <vector>
+#include <string>
+#include "../structures/record.hpp"
+
+void copyToString(string str, char * attribute);
+void setRecord(std::vector<std::string> row);
+void readFile();
+
+#endif
